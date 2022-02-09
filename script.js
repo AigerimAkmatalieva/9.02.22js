f2e3ac5c49614244d49a35da52c9354fcf692fa0
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-02-09 14:22:32 +0600

    added events button

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ let example3Button = document.getElementById('example3');
 example3Button.addEventListener('click', function() {
 alert("Bye");
 });
+example3Button.onclick = function() {
+  alert("Nice to meet you");
+}
